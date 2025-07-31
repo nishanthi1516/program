190b8d83169b1bc293cbc3195a2d0e5545ba7b6b
nishanthi1516 <mk.nishanthi007@gmail.com>
2025-07-31 15:57:38 +0530

    Create task6.py
    sum

---
+++ task6.py
@@ -0,0 +1,9 @@
+Algorithm:
+1.Start the program
+2.Get the n value
+3.Initialise i=0 and sum=0,n=10
+4.perform from step5 to step6 until
+5.increment the steps one by one
+6.sum 1=i
+7.print the sum
+8.stop the program
